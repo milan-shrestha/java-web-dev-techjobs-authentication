852d69c55d4d1ce8b7076161d5912caaa81e59c2
Milan Shrestha <milanshrestha17@hotmail.com>
2020-11-02 21:06:02 -0500

    Created AuthenticationFilter class and had it inherit from HandlerInterceptorAdapter

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -0,0 +1,6 @@
+package org.launchcode.javawebdevtechjobsauthentication;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+}
